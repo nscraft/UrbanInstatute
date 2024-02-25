71b9b19dc9751114aff6309eeec9932f4c382872
cleandeceit <nscraft@gmail.com>
2024-02-25 09:38:11 -0700

    wpi

--- study.py
+++ study.py
@@ -1,6 +1,14 @@
-import api_directory
-import api_ed_facts
+import pandas as pd
 
-directory = api_directory.results_df
-facts = api_ed_facts.results_df
+import api_CCD
+import api_EDFACTS
 
+CCD = api_CCD.results_df
+EDFACTS = api_EDFACTS.results_df
+
+CCD['teacher_student_ratio'] = CCD['teachers_fte'] / CCD['enrollment']
+
+merge_df = pd.merge(CCD, EDFACTS, on='ncessch', how='left')
+merge_df_filter = merge_df[merge_df['teacher_student_ratio'].notna() & merge_df['read_test_pct_prof_midpt'].notna()]
+
+print(merge_df_filter[['teacher_student_ratio', 'read_test_pct_prof_midpt']])
