3a011515e6e97299fbc6a2354391443806ae157c
cleandeceit <nscraft@gmail.com>
2024-02-25 09:46:33 -0700

    wpi

--- api_EDFACTS.py
+++ api_EDFACTS.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Endpoint URLs have the following format:
 # /api/v1/schools/edfacts/assessments/{year}/{grade_edfacts}/
 # results are limited to 10000 rows
-url = "https://educationdata.urban.org/api/v1/schools/edfacts/assessments/2014/grade-8/"
+url = "https://educationdata.urban.org/api/v1/schools/edfacts/assessments/2021/grade-8/"
 response = urlopen(url)
 data = loads(response.read())
 
--- study.py
+++ study.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import plotly.express as px
 import api_CCD
 import api_EDFACTS
 
@@ -11,4 +11,13 @@ CCD['teacher_student_ratio'] = CCD['teachers_fte'] / CCD['enrollment']
 merge_df = pd.merge(CCD, EDFACTS, on='ncessch', how='left')
 merge_df_filter = merge_df[merge_df['teacher_student_ratio'].notna() & merge_df['read_test_pct_prof_midpt'].notna()]
 
-print(merge_df_filter[['teacher_student_ratio', 'read_test_pct_prof_midpt']])
+print(merge_df_filter[['ncessch', 'teacher_student_ratio', 'read_test_pct_prof_midpt']])
+
+fig = px.scatter(merge_df_filter, x='read_test_pct_prof_midpt', y='teacher_student_ratio', text='ncessch')
+fig.update_layout(
+    title="Teacher Student Ratio vs. Reading Test Proficiency",
+    xaxis_title="Reading Test Proficiency",
+    yaxis_title="Teacher Student Ratio"
+)
+
+fig.show()
