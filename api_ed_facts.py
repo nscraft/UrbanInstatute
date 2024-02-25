0487a5bd3ae60717ba254676f614a8ba5370cd68
cleandeceit <nscraft@gmail.com>
2024-02-25 08:48:35 -0700

    wpi

--- api_ed_facts.py
+++ api_ed_facts.py
@@ -12,5 +12,3 @@ data = loads(response.read())
 jsonData = dict(data)
 results_data = jsonData['results']
 results_df = pd.DataFrame.from_records(results_data)
-
-print(results_df)
---
+++ study.py
@@ -0,0 +1,6 @@
+import api_directory
+import api_ed_facts
+
+directory = api_directory.results_df
+facts = api_ed_facts.results_df
+
